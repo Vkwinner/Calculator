0d5343a2ccf4433bc209c65558afd7a11dc050bd
Vkwinner <vkbedi25@gmail.com>
2024-01-22 15:03:08 +0530

    chat component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import Navbar from './components/Navbar';
-import Calculator from './components/Calculator';
-import Chatify from './components/Chatify';
-import FirstLinkComponent from './components/FirstLinkComponent';
-
-
-
 
 export default function App() {
   return (
--- src/components/Chatify.js
+++ src/components/Chatify.js
@@ -4,19 +4,22 @@ import chatbotmsg from "../data/demoData";
 const Chatify = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
-
+  const [value,setValue]=useState(0);
+  let i = 0;
   const handleSendMessage = () => {
     if (newMessage.trim() !== '') {
       setMessages([...messages, { content: newMessage, type: 'sent' }]);
       setNewMessage('');
-      // setTimeout(()=>{
-      //   setMessages([chatbotmsg[0],{content: chatbotmsg[0], type: 'incoming'}])
-      // },3000);
+      setTimeout(()=>{
+        setValue(i);
+        setMessages([chatbotmsg[1],{content: chatbotmsg[value], type: 'incoming'}])
+        setValue(i++);
+      },3000);
     }
   };
 
   return (
-    <div className="d-flex p-2 justify-content-center align-items-center mt-5 mx-5 border border-success border-2 rounded-5">
+    <div className="d-flex p-2 justify-content-center align-items-center mt-5 mx-5 rounded-5">
       <div className="border border-success border-2 rounded-4"style={{ width: '300px'}}>
         <div className="p-2  " style={{ maxHeight: '200px', overflowY: 'auto' }} >
           {messages.map((message, index) => (
