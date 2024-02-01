da9bda43a51d7ce9c4611536cc49b9a484f051a6
Vkwinner <vkbedi25@gmail.com>
2024-02-01 14:52:44 +0530

    updated UI

--- src/components/Chatify.js
+++ src/components/Chatify.js
@@ -73,17 +73,17 @@ const Chatify = () => {
                 className={' d-flex flex-column text-break'}
               >
 
-                <div className='d-flex justify-content-end align-items-center border border-success rounded-4  p-2 mx-2 mt-2 ' style={{ color: 'rgb(54, 82, 173)' }}>
-                  <div className='me-auto align-self-end' style={{fontSize:'12px' }}>{moment().format("LT")}</div>
-                  <div>{message.user} </div>
-                  <div className='ms-1'><IoPerson /></div>
-                </div>
                 {message.bot &&
                   <div className='d-flex justify-content-start align-items-center border border-success rounded-4  p-2 mx-2 mt-2 ' style={{ color: 'rgb(120, 148, 97)' }}>
                     <div className='me-1'><FcAssistant /></div>
                     <div >{message.bot}</div>
                     <div className='ms-auto align-self-end ' style={{fontSize:'12px' }}>{moment().format("LT")}</div>
                   </div>}
+                <div className='d-flex justify-content-end align-items-center border border-success rounded-4  p-2 mx-2 mt-2 ' style={{ color: 'rgb(54, 82, 173)' }}>
+                  <div className='me-auto align-self-end' style={{fontSize:'12px' }}>{moment().format("LT")}</div>
+                  <div>{message.user} </div>
+                  <div className='ms-1'><IoPerson /></div>
+                </div>
               </div>
             ))}
           </div>
--- src/hooks/useBotMessage.js
+++ src/hooks/useBotMessage.js
@@ -22,5 +22,3 @@ const useBotMessage = () => {
 };
 
 export default useBotMessage;
-
-
